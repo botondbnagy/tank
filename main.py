27798a60e82ff6791af8e277700c74086fe39064
Czumbel <pczumbel@gmail.com>
2018-04-21 18:28:32 +0200

    asdf

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ grey=(147, 147, 147)
 font = pygame.font.Font(None, 36)
 def add(a, b):
     return (a[0] + b[0], a[1] + b[1])
-def triadd(a, b, c):
-    return (a[0] + math.cos(c)*b[0], a[1] + math.cos(c)*b[1])
 class tank:
     class red:
         spd = 0
@@ -34,8 +32,12 @@ class tank:
         redf = pygame.transform.scale(redimage, (100, 100))
         def update():
             tank.red.angle += tank.red.angSpd
-            tank.red.pos = triadd(tank.red.pos, tank.red.vel, tank.red.angle)
-            surface.blit(tank.red.redf, tank.red.pos) 
+            tank.red.vel = (math.sin(math.radians(tank.red.angle))*tank.red.spd, math.cos(math.radians(tank.red.angle))*tank.red.spd)
+            tank.red.pos = add(tank.red.pos, tank.red.vel)
+        def draw():
+            blitted = pygame.transform.rotate(tank.red.redf, tank.red.angle)
+            tank_rect = blitted.get_rect(center=tank.red.pos)
+            surface.blit(blitted, tank_rect)
     class blue:
         spd = 0
         angle = 0
@@ -46,8 +48,13 @@ class tank:
         blueimage = pygame.image.load(picture)
         bluef = pygame.transform.scale(blueimage, (100, 100))
         def update():
+            tank.blue.angle += tank.blue.angSpd
+            tank.blue.vel = (math.sin(math.radians(tank.blue.angle))*tank.blue.spd, math.cos(math.radians(tank.blue.angle))*tank.blue.spd)
             tank.blue.pos = add(tank.blue.pos, tank.blue.vel)
-            surface.blit(tank.blue.bluef, tank.blue.pos) 
+        def draw():
+            blitted = pygame.transform.rotate(tank.blue.bluef, tank.blue.angle)
+            tank_rect = blitted.get_rect(center=tank.blue.pos)
+            surface.blit(blitted, tank_rect)
 
 def main():
     global surface
@@ -60,30 +67,48 @@ def main():
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == K_UP:
-                    tank.red.vel = (0, -1)
+                    tank.red.spd = -2
                 if event.key == K_DOWN:
-                    tank.red.vel = (0, 1)
+                    tank.red.spd = 2
                 if event.key == K_RIGHT:
-                    pass
+                    tank.red.angSpd = -4
                 if event.key == K_LEFT:
-                    pass
+                    tank.red.angSpd = 4
+                if event.key == K_w:
+                    tank.blue.spd = -2
+                if event.key == K_s:
+                    tank.blue.spd = 2
+                if event.key == K_d:
+                    tank.blue.angSpd = -4
+                if event.key == K_a:
+                    tank.blue.angSpd = 4
                 
             if event.type == KEYUP:
                 if event.key == K_UP:
-                    tank.red.vel = (0, 0)
+                    tank.red.spd = 0
                 if event.key == K_DOWN:
-                    tank.red.vel = (0, 0)
+                    tank.red.spd = 0
                 if event.key == K_RIGHT:
-                    pass
+                    tank.red.angSpd = 0
                 if event.key == K_LEFT:
-                    pass
+                    tank.red.angSpd = 0
+                if event.key == K_w:
+                    tank.blue.spd = 0
+                if event.key == K_s:
+                    tank.blue.spd = 0
+                if event.key == K_d:
+                    tank.blue.angSpd = 0
+                if event.key == K_a:
+                    tank.blue.angSpd = 0
             if event.type == VIDEORESIZE:
                         winH = event.h
                         winW = event.w
                         surface = pygame.display.set_mode((winW, winH), RESIZABLE)
         surface.fill((white))
         tank.blue.update()
+        tank.blue.draw()
         tank.red.update()
+        tank.red.draw()
         pygame.display.update()
         fpsClock.tick(FPS)
 main()
